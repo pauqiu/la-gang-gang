16bdf3b25e9ebb55c1211c81b1c722689413df2f
pauqiu <166964084+pauqiu@users.noreply.github.com>
2025-09-14 20:15:31 +0000

    macros definition

--- include/filesystem.h
+++ include/filesystem.h
@@ -4,7 +4,17 @@
 #include <fstream>
 #include <string>
 
-const int BLOCK_SIZE = 255;
+// --- MACROS ---
+
+#define BLOCK_SIZE 256
+#define MAX_BLOCKS 8192
+#define DISK_SIZE (BLOCK_SIZE * MAX_BLOCKS)
+#define MAX_INODES 1638
+#define MAX_DIR_ENTRIES 16
+#define DIRECT_POINTERS 12
+#define MAX_FILE_NAME 12
+
+// --- STRUCTS ---
 
 // Volume Control Block
 struct SuperBlock
@@ -12,15 +22,23 @@ struct SuperBlock
   int totalBlocks;
   int blockSize;
   int freeBlocksCount;
-  int freeBlocks[1000];
+  int freeBlocks[MAX_BLOCKS];
+
   int freeInnodesCount;
-  int freeInnodes[1000];
+  int freeInnodes[MAX_INODES];
+};
+
+struct dirEntry
+{
+  char name[MAX_FILE_NAME];
+  int inode;
 };
 
 // Special kind of file that maps names to inodes
 struct Directory
 {
-  int indexBlock[1000]; 
+  int entriesCount;
+  dirEntry entries[MAX_DIR_ENTRIES];
 };
 
 // Data of the file
@@ -30,18 +48,18 @@ struct Block
   
 };
 
-const int DIRECT_POINTERS = 10;
-
 // File Control Block
 struct Innode
 {
   int id;
   int type; // 0 for file, 1 for directory
   int size;
-  std::string permissions;
+  char permissions[4];
   int directPointers[DIRECT_POINTERS];
   int indirectPointer;
 };
+
+// --- CLASS ---
 
 class FileSystem {
   // Attributes
--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -1,29 +1,37 @@
 #include "filesystem.h"
 
-FileSystem::FileSystem(std::string diskName) 
-{
-  this->diskName = diskName;
+#include <iostream>
 
+FileSystem::FileSystem(std::string diskName) : diskName(diskName)
+{
   // Check if the disk file already exists.
   this->diskFile.open(diskName, std::ios::in | std::ios::out | std::ios::binary);
   
-  if (!this->diskFile) {
+  if (!this->diskFile.is_open()) {
+    std::cout << "Disk does not exist. Creating a new disk..." << std::endl;
     initializeDisk();
-  } 
+  }
    
 }
 
 FileSystem::~FileSystem() 
 {
-  
+  if (this->disk_file.is_open()) {
+    this->disk_file.close();
+  }
 }
 
 void FileSystem::initializeDisk()
 {
   // Create a new file stream object for the disk file.
-  std::ofstream newDiskFile(this->diskName, std::ios::in | std::ios::binary);
+  std::ofstream newDiskFile(this->diskName, std::ios::out | std::ios::binary);
 
-  // TODO: Initialize the disk file.
+  if (!newDiskFile.is_open()) {
+    std::cerr << "Error creating disk file." << std::endl;
+  } else {
+    // TODO: Initialize the disk file.
+  }
+
 }
 
 bool FileSystem::createFile(const std::string fileName)
