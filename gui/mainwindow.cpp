cf2c594c3901e76ee006f0196e84d7b16c3a8aa7
Dylan Torres <dylantorres538@gmail.com>
2025-09-24 22:04:22 -0600

    Log in page (UI) implemented

---
+++ gui/mainwindow.cpp
@@ -0,0 +1,48 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "menuwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setFixedSize(1100, 700);
+    ui->usernameMessage->setVisible(false);
+    ui->passwordMessage->setVisible(false);
+    ui->passwordHelp->setVisible(false);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_logInButton_clicked()
+{
+    if(ui->usernameInput->text() != "" && ui->passwordInput->text() != ""){
+        menuWindow *menu = new menuWindow();
+        menu->setLogInWindow(this);
+        menu->setUsername(ui->usernameInput->text());
+        menu->setUserRole("admin");
+        menu->show();
+        close();
+    } else{
+        if(ui->usernameInput->text() == ""){
+            ui->usernameMessage->setVisible(true);
+        } else{
+            ui->usernameMessage->setVisible(false);
+        }
+        if(ui->passwordInput->text() == ""){
+            ui->passwordMessage->setVisible(true);
+        } else {
+            ui->passwordMessage->setVisible(false);
+        }
+    }
+}
+
+
+void MainWindow::on_forgotPasswordButton_clicked()
+{
+    ui->passwordHelp->setVisible(true);
+}
