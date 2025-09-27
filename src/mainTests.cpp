e788da32f52d4de795d99cb2fd2c26f0cb95b716
joseg <jose.granadosduarte@ucr.ac.cr>
2025-09-27 15:17:18 -0400

    editFIle testing

--- src/mainTests.cpp
+++ src/mainTests.cpp
@@ -26,9 +26,22 @@ int main() {
         fs.readFile("test.txt");
         std::cout << std::endl;
 
+        std::cout << "\nTesting editFile Methods:\n";
+        
+        // replace in specific position
+        std::cout << "Test 1: Replace 'Hello' with 'Hi' at position 0\n";
+        fs.editFile("test.txt", 0, "Hi");
+        std::cout << "After replace: ";
+        fs.readFile("test.txt");
+        std::cout << std::endl;
+        
+        
+        std::cout << "\nChecking file was deleted correctly: \n";
         // Clean up
         fs.deleteFile("test.txt");
 
+        // Check if file was deleted correctly.
+        fs.readFile("test.txt");
     } else {
         std::cout << "Failed to create file\n";
     }
